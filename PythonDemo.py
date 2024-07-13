71d8db3fd6055dfdbe8ed8323e11eca8f3e96298
MedlejC <charbel.medlej@lau.edu>
2024-07-13 12:45:38 +0300

    Add explanation

--- PythonDemo.py
+++ PythonDemo.py
@@ -4,7 +4,16 @@
 # Part 2:
 import pandas as pd
 
+# This block prints the actual content of the csv file to the terminal
+# First we read the CSV file into a pandas DataFrame (df)
+# Then we set the display options by enforcing a maximum number of columns to be displayed in the terminal
+# In that way, I ensure that all columns are printed out
 users = "./users.csv"
 df = pd.read_csv(users)
 pd.options.display.max_columns = len(df.columns)
+print("----------------------------------")
+print("Users Table:")
+print("----------------------------------")
 print(df)
+print()
+
